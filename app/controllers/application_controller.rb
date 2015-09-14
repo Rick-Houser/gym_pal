ac2d6cad1e90b8f8cbcfa60e63d59598344217c6
FrackenToaster <frakentoaster@gmail.com>
2015-09-14 12:02:25 -0700

    Modified devise to use stripe API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
+  
+  before_filter :configure_permitted_paramaters, if: :devise_controller?
+
+  protected
+  def configure_permitted_paramaters
+  	devise_paramater_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation)}
+  end
 end

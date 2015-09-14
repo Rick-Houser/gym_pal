4f6f793cc7b581c1090221883008393ca340ab53
FrackenToaster <frakentoaster@gmail.com>
2015-09-14 13:00:17 -0700

    Add profile model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :workouts
   belongs_to :plan
+  has_one :profile
   attr_accessor :stripe_card_token
 
   def save_with_payment

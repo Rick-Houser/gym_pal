9d6ecccedf10b86292892c0ff705c04cefc09e64
FrackenToaster <frakentoaster@gmail.com>
2015-09-14 22:59:15 -0700

    Conditional root if valid user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
-
+  resources :contacts, only: [:new, :create]
   resources :workouts
   resources :users do 
   	resource :profile
   end
-  resources :contacts, only: [:new, :create]
+
+  authenticated :user do 
+  	root 'workouts#index', as: "authenticated_root"
+  end
+
   get '/about' => 'pages#about'
 
   root 'pages#home'

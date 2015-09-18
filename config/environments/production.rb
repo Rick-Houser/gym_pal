73d01cb7290115c9e0272fcf71ba9e7199eb8831
FrackenToaster <frakentoaster@gmail.com>
2015-09-18 13:58:58 -0700

    Upload profile images via paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,10 @@ Rails.application.configure do
   # Amazon Web Services S3
   config.paperclip_defaults = {
     :storage => :s3,
-    :bucket => 'web-folio'
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
 end

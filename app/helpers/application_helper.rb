37c4a03928bc1a42884dee116b911266f14f2b7f
FrackenToaster <frakentoaster@gmail.com>
2015-09-22 18:54:14 -0700

    Add newrelic.yml for activity monitoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-	def s3_image_path(filename)
-		"https://s3-us-west-2.amazonaws.com/web-folio/#{filename}"
-	end
-
-	def s3_image_tag(filename, options={})
-		image_tag(s3_image_path(filename), options)
-	end
 end
